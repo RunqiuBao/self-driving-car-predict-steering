0f55efe6c988bba6e9357d21069d80172d28b579
akshatbjain <akshatbjain93@gmail.com>
2016-12-03 05:30:50 -0500

    Added time stamp to weights

--- model/scripts/train_single_model.py
+++ model/scripts/train_single_model.py
@@ -7,6 +7,7 @@ from keras.models import model_from_json
 import load_data
 import numpy
 import matplotlib.pyplot as plt
+import time
 
 # load json model
 json_file = open('model.json', 'r')
@@ -15,28 +16,32 @@ json_file.close()
 model = model_from_json(loaded_model)
 print "Loaded the model"
 
+# complie the model
 model.compile(loss='mse', optimizer='adam')
 
+# set batch_size and number of epochs
 batch_size = 500
 epochs = 10
 
+# get the values of correct steering angels
 y_train_data = load_data.loadY()
 
+# training the model
 for i in range(epochs):
     trainPredict = numpy.empty([0])
     for j in range(load_data.len_train_samples/batch_size):
-        x_train, y_train = load_data.loadTrainData(batch_size)
+        x_train, y_train = load_data.loadTrainData(batch_size, 'all')
         history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
         trainPredict = numpy.append(trainPredict, model.predict(x_train))
         print "Epoch" + str(i+1)
 
-    x_train, y_train = load_data.loadTrainData(load_data.len_train_samples - (load_data.train_batch_index%load_data.len_train_samples))
+    x_train, y_train = load_data.loadTrainData(load_data.len_train_samples - (load_data.train_batch_index%load_data.len_train_samples), 'all')
     history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
     trainPredict = numpy.append(trainPredict, model.predict(x_train))
 
 trainPredict = numpy.expand_dims(trainPredict, axis = 1)
 
-# Estimate model performance
+# estimate model performance
 trainScore = model.evaluate(x_train, y_train, verbose=2)
 print 'Train Score: ', trainScore
 
@@ -52,6 +57,8 @@ plt.legend(loc = 'upper left')
 plt.savefig('Performance')
 plt.show()
 
+# get time stamp
+timestr = time.strftime("%Y%m%d-%H%M%S")
 # serialize weights to HDF5
-model.save_weights("model_epochs_1-5.h5")
+model.save_weights("weights-all-" + timestr + ".h5")
 print("Saved weights to disk")
