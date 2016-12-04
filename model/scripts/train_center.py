c09573ef8178ad7d6638dc74a15e6cf41ceaaba2
akshatbjain <akshatbjain93@gmail.com>
2016-12-03 20:31:03 -0500

    Made changes as per changes in load_data. Also added feature of saving weights and plot only if the user wants to.

--- model/scripts/train_center.py
+++ model/scripts/train_center.py
@@ -24,18 +24,18 @@ batch_size = 500
 epochs = 10
 
 # get the values of correct steering angels
-y_train_data = load_data.loadY()
+y_train_data = load_data.loadY("center")
 
 # training the model
 for i in range(epochs):
     trainPredict = numpy.empty([0])
-    for j in range(load_data.len_train_samples/batch_size):
-        x_train, y_train = load_data.loadTrainData(batch_size, 'center')
+    for j in range(load_data.clen_train/batch_size):
+        x_train, y_train = load_data.loadTrainDataC(batch_size)
         history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
         trainPredict = numpy.append(trainPredict, model.predict(x_train))
         print "Epoch" + str(i+1)
 
-    x_train, y_train = load_data.loadTrainData(load_data.len_train_samples - (load_data.train_batch_index%load_data.len_train_samples), 'center')
+    x_train, y_train = load_data.loadTrainDataC(load_data.clen_train - (load_data.ctrain_batch_index%load_data.clen_train))
     history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
     trainPredict = numpy.append(trainPredict, model.predict(x_train))
 
@@ -50,15 +50,21 @@ trainPredictPlot = numpy.empty_like(y_train_data)
 trainPredictPlot[:, :] = numpy.nan
 trainPredictPlot[0:len(trainPredict), :] = trainPredict
 
-# plot baseline and predictions
-plt.plot(y_train_data, label = 'Dataset')
-plt.plot(trainPredictPlot, label = 'Training Prediction')
-plt.legend(loc = 'upper left')
-plt.savefig('Performance')
-plt.show()
+input_1 = raw_input("Do you want to save the plot? (y/n): ")
+input_2 = raw_input("Do you want to save the weights? (y/n): ")
 
 # get time stamp
 timestr = time.strftime("%Y%m%d-%H%M%S")
-# serialize weights to HDF5
-model.save_weights("weights-center-" + timestr + ".h5")
-print("Saved weights to disk")
+
+if input_1 == 'y':
+    # plot baseline and predictions
+    plt.plot(y_train_data, label = 'Dataset')
+    plt.plot(trainPredictPlot, label = 'Training Prediction')
+    plt.legend(loc = 'upper left')
+    plt.savefig('Training_Performance_Center_' + timestr)
+    print "Saved plot to disk"
+
+if input_2 == 'y':
+    # serialize weights to HDF5
+    model.save_weights("weights-center-" + timestr + ".h5")
+    print("Saved weights to disk")
