e14f27a5a6501c02971d7bbe66e1c3ebc4aa11de
akshatbjain <akshatbjain93@gmail.com>
2016-12-05 05:05:02 -0500

    Changed training style using generator

--- model/scripts/train_center.py
+++ model/scripts/train_center.py
@@ -9,6 +9,8 @@ import numpy
 import matplotlib.pyplot as plt
 import time
 
+epochs = 100
+
 # load json model
 json_file = open('model.json', 'r')
 loaded_model = json_file.read()
@@ -19,63 +21,49 @@ print "Loaded the model"
 # complie the model
 model.compile(loss='mse', optimizer='adam')
 
-# set batch_size and number of epochs
-batch_size = 500
-epochs = 200
+# generator
+genT = load_data.trainDataGen('center')
+genV = load_data.valDataGen('center')
 
 # get the values of correct steering angels
 y_train_data = load_data.loadY("center")
 
-#Empty array to record loss at the end of every epoch
-loss = numpy.empty([0])
-
 # training the model
-for i in range(epochs):
-    trainPredict = numpy.empty([0])
-    for j in range(load_data.clen_train/batch_size):
-        x_train, y_train = load_data.loadTrainDataC(batch_size)
-        history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
-        trainPredict = numpy.append(trainPredict, model.predict(x_train))
-        print "Epoch" + str(i+1)
+history = model.fit_generator(genT, samples_per_epoch = load_data.clen_train, nb_epoch = epochs, verbose = 1, nb_worker = 1)
+print "Entering Prediction please wait... Your plots will be generated soon..."
+trainPredict = model.predict_generator(genV, val_samples = load_data.clen_val, nb_worker = 1)
 
-    x_train, y_train = load_data.loadTrainDataC(load_data.clen_train - (load_data.ctrain_batch_index%load_data.clen_train))
-    history = model.fit(x_train, y_train, nb_epoch = 1, verbose = 2)
-    trainPredict = numpy.append(trainPredict, model.predict(x_train))
-    loss = numpy.append(loss, history.history['loss'])
-    print "Epoch" + str(i+1)
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(y_train_data)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[0:len(trainPredict), :] = trainPredict
 
-    trainPredict = numpy.expand_dims(trainPredict, axis = 1)
+#Plotting steering angle actual vs predicted
+plt.figure(0)
+plt.plot(y_train_data, label = 'Actual Dataset')
+plt.plot(trainPredictPlot, label = 'Training Prediction')
+plt.title('Steering Angle: Actual vs Predicted')
+plt.xlabel('Number of images')
+plt.ylabel('Steering angle in radians')
+plt.legend(loc = 'upper left')
+plt.savefig('Training_Steering_Angle_Center')
+print "Saved steering angle plot to disk"
+plt.close()
 
-    # shift train predictions for plotting
-    trainPredictPlot = numpy.empty_like(y_train_data)
-    trainPredictPlot[:, :] = numpy.nan
-    trainPredictPlot[0:len(trainPredict), :] = trainPredict
-
-    #Plotting steering angle actual vs predicted
-    plt.figure(0)
-    plt.plot(y_train_data, label = 'Actual Dataset')
-    plt.plot(trainPredictPlot, label = 'Training Prediction')
-    plt.title('Steering Angle: Actual vs Predicted for epoch ' + str(i + 1))
-    plt.xlabel('Number of images')
-    plt.ylabel('Steering angle in radians')
-    plt.legend(loc = 'upper left')
-    plt.savefig('Training_Steering_Angle_Center_epoch_' + str(i + 1))
-    print "Saved steering angle plot to disk"
-    plt.close()
+# summarize history for loss
+plt.figure(1)
+plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train'], loc='upper left')
+plt.savefig('Loss_Plot_Center')
+print "Saved loss plot to disk"
+plt.close()
 
 # get time stamp
 timestr = time.strftime("%Y%m%d-%H%M%S")
-
-#Plot loss
-plt.figure(1)
-plt.plot(loss, label = 'Loss')
-plt.title('Change in Loss over number of epochs')
-plt.xlabel('Number of epochs')
-plt.ylabel('Loss')
-plt.legend(loc = 'upper left')
-plt.savefig('Training_Loss_Center_' + timestr)
-print "Saved loss plot to disk"
-plt.close()
 
 # serialize weights to HDF5
 model.save_weights("weights-center-" + timestr + ".h5")
