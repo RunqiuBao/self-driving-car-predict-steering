9d12a25ddc4d7206ce04f9c3f69ed6137a8aecea
akshatbjain <akshatbjain93@gmail.com>
2016-12-06 16:00:30 -0500

    Added Xavier initialization (called glorot_uniform in keras) Added bias to all layers Removed tanh activation from output layer

--- model/scripts/model.py
+++ model/scripts/model.py
@@ -7,34 +7,34 @@ from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
 model = Sequential()
 
 #First Convolutional Layer with stride of 2x2 and kernel size 5x5
-model.add(Convolution2D(24, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', input_shape=(3,120,160)))
+model.add(Convolution2D(24, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', input_shape=(3,120,160), init="glorot_uniform", bias = True))
 model.add(SpatialDropout2D(0.5))
 
 #Second Convolutional Layer with stride of 2x2 and kernel size 5x5
-model.add(Convolution2D(36, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same'))
+model.add(Convolution2D(36, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
 model.add(SpatialDropout2D(0.5))
 
 #Third Convolutional Layer with stride of 2x2 and kernel size 5x5
-model.add(Convolution2D(48, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same'))
+model.add(Convolution2D(48, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
 model.add(SpatialDropout2D(0.5))
 
 #Fourth Convolutional Layer with no stride and kernel size 3x3
-model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same'))
+model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
 model.add(SpatialDropout2D(0.5))
 
 #Fifth Convolutional Layer with no stride and kernel size 3x3
-model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same'))
+model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
 
 model.add(Flatten())
-model.add(Dense(1164, activation='relu'))
+model.add(Dense(1164, activation='relu', init="glorot_uniform", bias = True))
 model.add(Dropout(0.5))
-model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu', init="glorot_uniform", bias = True))
 model.add(Dropout(0.5))
-model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu', init="glorot_uniform", bias = True))
 model.add(Dropout(0.5))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu', init="glorot_uniform", bias = True))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation='tanh'))
+model.add(Dense(1))
 model.summary()
 
 model_json = model.to_json()
