1bc992bd75bd27bb95a577619500db7e24a5d54c
Akshat Bharat Jain <akshatbjain93@gmail.com>
2016-12-14 22:16:39 -0500

    Removed dropout from convolution layers.
    Also changed dropout of dense layers from 0.5 to 0.8.

--- model/scripts/model_merged.py
+++ model/scripts/model_merged.py
@@ -11,48 +11,48 @@ right_model = Sequential()
 
 #First Convolutional Layer with stride of 2x2 and kernel size 5x5
 left_model.add(Convolution2D(24, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', input_shape=(3,120,160), init="glorot_uniform", bias = True))
-left_model.add(SpatialDropout2D(0.5))
+#left_model.add(SpatialDropout2D(0.5))
 #Second Convolutional Layer with stride of 2x2 and kernel size 5x5
 left_model.add(Convolution2D(36, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-left_model.add(SpatialDropout2D(0.5))
+#left_model.add(SpatialDropout2D(0.5))
 #Third Convolutional Layer with stride of 2x2 and kernel size 5x5
 left_model.add(Convolution2D(48, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-left_model.add(SpatialDropout2D(0.5))
+#left_model.add(SpatialDropout2D(0.5))
 #Fourth Convolutional Layer with no stride and kernel size 3x3
 left_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
-left_model.add(SpatialDropout2D(0.5))
+#left_model.add(SpatialDropout2D(0.5))
 #Fifth Convolutional Layer with no stride and kernel size 3x3
 left_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
 left_model.add(Flatten())
 
 #First Convolutional Layer with stride of 2x2 and kernel size 5x5
 center_model.add(Convolution2D(24, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', input_shape=(3,120,160), init="glorot_uniform", bias = True))
-center_model.add(SpatialDropout2D(0.5))
+#center_model.add(SpatialDropout2D(0.5))
 #Second Convolutional Layer with stride of 2x2 and kernel size 5x5
 center_model.add(Convolution2D(36, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-center_model.add(SpatialDropout2D(0.5))
+#center_model.add(SpatialDropout2D(0.5))
 #Third Convolutional Layer with stride of 2x2 and kernel size 5x5
 center_model.add(Convolution2D(48, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-center_model.add(SpatialDropout2D(0.5))
+#center_model.add(SpatialDropout2D(0.5))
 #Fourth Convolutional Layer with no stride and kernel size 3x3
 center_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
-center_model.add(SpatialDropout2D(0.5))
+#center_model.add(SpatialDropout2D(0.5))
 #Fifth Convolutional Layer with no stride and kernel size 3x3
 center_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
 center_model.add(Flatten())
 
 #First Convolutional Layer with stride of 2x2 and kernel size 5x5
 right_model.add(Convolution2D(24, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', input_shape=(3,120,160), init="glorot_uniform", bias = True))
-right_model.add(SpatialDropout2D(0.5))
+#right_model.add(SpatialDropout2D(0.5))
 #Second Convolutional Layer with stride of 2x2 and kernel size 5x5
 right_model.add(Convolution2D(36, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-right_model.add(SpatialDropout2D(0.5))
+#right_model.add(SpatialDropout2D(0.5))
 #Third Convolutional Layer with stride of 2x2 and kernel size 5x5
 right_model.add(Convolution2D(48, 5, 5, activation='relu', subsample = (2, 2), border_mode = 'same', init="glorot_uniform", bias = True))
-right_model.add(SpatialDropout2D(0.5))
+#right_model.add(SpatialDropout2D(0.5))
 #Fourth Convolutional Layer with no stride and kernel size 3x3
 right_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
-right_model.add(SpatialDropout2D(0.5))
+#right_model.add(SpatialDropout2D(0.5))
 #Fifth Convolutional Layer with no stride and kernel size 3x3
 right_model.add(Convolution2D(64, 3, 3, activation='relu', border_mode = 'same', init="glorot_uniform", bias = True))
 right_model.add(Flatten())
@@ -62,13 +62,13 @@ merged = Merge([left_model, center_model, right_model], mode='concat')
 model = Sequential()
 model.add(merged)
 model.add(Dense(1164, activation='relu', init="glorot_uniform", bias = True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.8))
 model.add(Dense(100, activation='relu', init="glorot_uniform", bias = True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.8))
 model.add(Dense(50, activation='relu', init="glorot_uniform", bias = True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.8))
 model.add(Dense(10, activation='relu', init="glorot_uniform", bias = True))
-model.add(Dropout(0.5))
+model.add(Dropout(0.8))
 model.add(Dense(1))
 model.summary()
 
